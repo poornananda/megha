697f9310fb079587dceff9f8e42fa7e0e0782d3d
megharajmk <127726310+megharajmk@users.noreply.github.com>
2023-03-14 10:58:52 +0530

    Update graphModule2.js

--- src/graphs/graphModule2.js
+++ src/graphs/graphModule2.js
@@ -23,7 +23,7 @@ export const GraphsModule2 = () => {
             <div className="speedometer-wrapper1">
               <ReactSpeedometer
                 maxValue={100}
-                value={45}
+                value={18}
                 segments={10}
                 needleColor="black"
                 startColor="red"
@@ -38,7 +38,7 @@ export const GraphsModule2 = () => {
             <div className="speedometer-wrapper">
               <ReactSpeedometer
                 maxValue={600}
-                value={400}
+                value={600}
                 segments={10}
                 needleColor="black"
                 startColor="green"
@@ -50,7 +50,7 @@ export const GraphsModule2 = () => {
             </div>
             <div className="speedometer-wrapper">
               <ReactSpeedometer
-                value={180}
+                value={222}
                 maxSegmentLabels={5}
                 segments={1000}
                 maxValue={222}
@@ -81,7 +81,7 @@ export const GraphsModule2 = () => {
             <div className="speedometer-wrapper">
               <ReactSpeedometer
                 maxValue={0.3}
-                value={0.2}
+                value={0.15}
                 segments={6}
                 needleColor="black"
                 startColor="green"
@@ -93,7 +93,7 @@ export const GraphsModule2 = () => {
             </div>
             <div className="speedometer-wrapper">
               <ReactSpeedometer
-                value={37}
+                value={43.5}
                 segments={10}
                 maxValue={43.5}
                 currentValueText="Performance Metrics - A14"
